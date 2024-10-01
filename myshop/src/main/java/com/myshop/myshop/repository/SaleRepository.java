34bb4ab43eef46d5705c77411c13340464a422c7
Panu <qwsedrftgyhuiop1@gmail.com>
2024-10-01 23:09:18 +0700

    update sql ผิด

--- myshop/src/main/java/com/myshop/myshop/repository/SaleRepository.java
+++ myshop/src/main/java/com/myshop/myshop/repository/SaleRepository.java
@@ -14,23 +14,23 @@ import java.util.List;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query(value = "SELECT p.id AS productId, p.name AS productName, p.stock AS productStock, " +
-            "p.cost_price_header AS costPriceHeader, p.retail_price AS retailPrice, " +
+            "s.cost_price_header AS costPriceHeader, s.sale_price AS retailPrice, " +
             "SUM(s.quantity) AS totalQuantity, " +
             "SUM(s.sale_price) AS totalSalePrice, " +
             "SUM(s.cost_price_header) AS totalCostPriceHeader " +
             "FROM product p " +
             "JOIN sale s ON p.id = s.productID " +
             "WHERE EXTRACT(MONTH FROM s.sale_date) = :month AND EXTRACT(YEAR FROM s.sale_date) = :year " +
-            "GROUP BY p.id, p.name, p.stock, p.cost_price_header, p.retail_price", nativeQuery = true)
+            "GROUP BY p.id, p.name, p.stock, s.cost_price_header, s.sale_price", nativeQuery = true)
     List<SaleSummary> findSaleSummaryByMonthAndYear(@Param("month") int month, @Param("year") int year);
 
     @Query(value = "SELECT p.id AS productId, p.name AS productName, p.stock AS productStock, " +
-            "p.cost_price_header AS costPriceHeader, p.retail_price AS retailPrice, " +
+            "s.cost_price_header AS costPriceHeader, s.sale_price AS retailPrice, " +
             "SUM(s.quantity) AS totalQuantity, " +
             "SUM(s.sale_price) AS totalSalePrice, " +
             "SUM(s.cost_price_header) AS totalCostPriceHeader " +
             "FROM product p " +
             "JOIN sale s ON p.id = s.productID " +
-            "GROUP BY p.id, p.name, p.stock, p.cost_price_header, p.retail_price", nativeQuery = true)
+            "GROUP BY p.id, p.name, p.stock, s.cost_price_header, s.sale_price", nativeQuery = true)
     List<SaleSummary> findAllSalesSummary();
 }
